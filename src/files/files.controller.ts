9aac71baca5587b38562b73ee5bbf8a0d5017d1b
avimi13 <alfredorequenez57libra@gmail.com>
2023-05-05 08:49:03 -0600

    changes

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,15 +1,33 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFIlter.helpers';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) { }
   @Post('product')
   @UseInterceptors(
-    FileInterceptor('file')
+    FileInterceptor('file',
+      {
+        fileFilter: fileFilter,
+        //definir donde se va a guardar el archivo
+        storage: diskStorage({
+          destination: './uploads/products',
+          filename: fileNamer,
+          // filename: (req, file, cb) => {
+          //   cb(null, file.originalname);
+          // }
+        })
+      })
   )
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
-    return file;
+    //si no viene la imagen que se mande el siguiente mensaje
+    if (!file) {
+      throw new BadRequestException('File is required');
+    }
+    return { fileName: file.originalname }
   }
 }
